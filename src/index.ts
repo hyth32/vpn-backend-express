de98f42c351bce6b2bbb91170033eb5ab2be46e5
Ivan Goryaev <5474n.sage@gmail.com>
2025-10-17 23:12:18 +0300

    добавил express

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { AppDataSource } from './data-source'
+import * as express from 'express'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
 
 AppDataSource.initialize()
     .then(async () => {
-        //
+        const port = Number(process.env.APP_PORT)
+        app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
     })
     .catch(error => console.log(error))

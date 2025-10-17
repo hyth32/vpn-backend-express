7aafef5d8f55d705b0999d95889bf5e89db209c2
Ivan Goryaev <5474n.sage@gmail.com>
2025-10-17 22:53:14 +0300

    добавил скрипты для генерации миграций

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    synchronize: false,
+    synchronize: process.env.NODE_ENV === 'development',
     logging: false,
     entities: ['src/entity/*.ts'],
     migrations: [],
